72b054f5a1cfb905f2c7e3e9bfad2473fa2836eb
Mentalista <81753672+leitura@users.noreply.github.com>
2022-01-02 23:18:53 -0300

    Add files via upload

--- v2ray.sh
+++ v2ray.sh
@@ -2,7 +2,7 @@
 #25/01/2021 by ngm
 clear
 clear
-SCPdir="/etc/VPS-MX"
+SCPdir="/etc/SSHPLUS"
 SCPfrm="${SCPdir}/herramientas" && [[ ! -d ${SCPfrm} ]] && exit
 SCPinst="${SCPdir}/protocolos"&& [[ ! -d ${SCPinst} ]] && exit
 declare -A cor=( [0]="\033[1;37m" [1]="\033[1;34m" [2]="\033[1;31m" [3]="\033[1;33m" [4]="\033[1;32m" )
@@ -30,7 +30,7 @@ intallv2ray () {
 apt install python3-pip -y 
 source <(curl -sL https://www.dropbox.com/s/ukkyksfdo3lqqmc/install-v2ray.sh)
 msg -ama "$(fun_trans "instalado com sucesso")!"
-USRdatabase="/etc/VPS-MX/RegV2ray"
+USRdatabase="/etc/SSHPLUS/RegV2ray"
 [[ ! -e ${USRdatabase} ]] && touch ${USRdatabase}
 sort ${USRdatabase} | uniq > ${USRdatabase}tmp
 mv -f ${USRdatabase}tmp ${USRdatabase}
@@ -74,7 +74,7 @@ ${SCPinst}/v2ray.sh
 }
 unistallv2 () {
 source <(curl -sL https://www.dropbox.com/s/ukkyksfdo3lqqmc/install-v2ray.sh) --remove > /dev/null 2>&1
-rm -rf /etc/VPS-MX/RegV2ray > /dev/null 2>&1
+rm -rf /etc/SSHPLUS/RegV2ray > /dev/null 2>&1
 echo -e "\033[1;92m                  V2RAY REMOVIDO OK "
 msg -bar
 msg -ne "Enter Para Continuar" && read enter
@@ -135,14 +135,14 @@ done
 valid=$(date '+%C%y-%m-%d' -d " +$diasuser days") && datexp=$(date "+%F" -d " + $diasuser days")
 echo -e "\e[91m >> Expira em: \e[92m$datexp "
 ##Registro
-echo "  $UUID | $nick | $valid " >> /etc/VPS-MX/RegV2ray
+echo "  $UUID | $nick | $valid " >> /etc/SSHPLUS/RegV2ray
 v2ray restart > /dev/null 2>&1
 echo ""
-v2ray info > /etc/VPS-MX/v2ray/confuuid.log
-lineP=$(sed -n '/'${UUID}'/=' /etc/VPS-MX/v2ray/confuuid.log)
+v2ray info > /etc/SSHPLUS/v2ray/confuuid.log
+lineP=$(sed -n '/'${UUID}'/=' /etc/SSHPLUS/v2ray/confuuid.log)
 numl1=4
 let suma=$lineP+$numl1
-sed -n ${suma}p /etc/VPS-MX/v2ray/confuuid.log 
+sed -n ${suma}p /etc/SSHPLUS/v2ray/confuuid.log 
 echo ""
 msg -bar
 echo -e "\e[92m           UUID ADICIONADO COM SUCESSO "
@@ -165,13 +165,13 @@ msg -tit
 msg -ama "             EXCLUIR USUÁRIO | UUID V2RAY"
 msg -bar
 echo -e "\e[97m               USUÁRIOS REGISTRADOS"
-echo -e "\e[33m$(cat /etc/VPS-MX/RegV2ray|cut -d '|' -f2,1)" 
+echo -e "\e[33m$(cat /etc/SSHPLUS/RegV2ray|cut -d '|' -f2,1)" 
 msg -bar
 echo -ne "\e[91m >> digite UUID para deletar:\n \033[1;92m " && read uuidel
 [[ $(sed -n '/'${uuidel}'/=' /etc/v2ray/config.json|head -1) ]] || invaliduuid
 lineP=$(sed -n '/'${uuidel}'/=' /etc/v2ray/config.json)
-linePre=$(sed -n '/'${uuidel}'/=' /etc/VPS-MX/RegV2ray)
-sed -i "${linePre}d" /etc/VPS-MX/RegV2ray
+linePre=$(sed -n '/'${uuidel}'/=' /etc/SSHPLUS/RegV2ray)
+sed -i "${linePre}d" /etc/SSHPLUS/RegV2ray
 numl1=2
 let resta=$lineP-$numl1
 sed -i "${resta}d" /etc/v2ray/config.json
@@ -192,11 +192,11 @@ msg -bar
 msg -tit
 msg -ama "         USUÁRIOS REGISTRADOS | UUID V2RAY"
 msg -bar
-# usersss=$(cat /etc/VPS-MX/RegV2ray|cut -d '|' -f1)
-# cat /etc/VPS-MX/RegV2ray|cut -d'|' -f3
+# usersss=$(cat /etc/SSHPLUS/RegV2ray|cut -d '|' -f1)
+# cat /etc/SSHPLUS/RegV2ray|cut -d'|' -f3
 VPSsec=$(date +%s)
-local HOST="/etc/VPS-MX/RegV2ray"
-local HOST2="/etc/VPS-MX/RegV2ray"
+local HOST="/etc/SSHPLUS/RegV2ray"
+local HOST2="/etc/SSHPLUS/RegV2ray"
 local RETURN="$(cat $HOST|cut -d'|' -f2)"
 local IDEUUID="$(cat $HOST|cut -d'|' -f1)"
 if [[ -z $RETURN ]]; then
@@ -209,14 +209,14 @@ i=1
 echo -e "\e[97m                 UUID                | USUÁRIO | EXPIRAÇÃO \e[93m"
 msg -bar
 while read hostreturn ; do
-DateExp="$(cat /etc/VPS-MX/RegV2ray|grep -w "$hostreturn"|cut -d'|' -f3)"
+DateExp="$(cat /etc/SSHPLUS/RegV2ray|grep -w "$hostreturn"|cut -d'|' -f3)"
 if [[ ! -z $DateExp ]]; then             
 DataSec=$(date +%s --date="$DateExp")
 [[ "$VPSsec" -gt "$DataSec" ]] && EXPTIME="\e[91m[EXPIRADO]\e[97m" || EXPTIME="\e[92m[$(($(($DataSec - $VPSsec)) / 86400))]\e[97m Dias"
 else
 EXPTIME="\e[91m[ S/R ]"
 fi 
-usris="$(cat /etc/VPS-MX/RegV2ray|grep -w "$hostreturn"|cut -d'|' -f2)"
+usris="$(cat /etc/SSHPLUS/RegV2ray|grep -w "$hostreturn"|cut -d'|' -f2)"
 local contador_secuencial+="\e[93m$hostreturn \e[97m|\e[93m$usris\e[97m|\e[93m $EXPTIME \n"           
       if [[ $i -gt 30 ]]; then
 	      echo -e "$contador_secuencial"
@@ -227,7 +227,7 @@ let i++
 done <<< "$IDEUUID"
 
 [[ ! -z $contador_secuencial ]] && {
-linesss=$(cat /etc/VPS-MX/RegV2ray | wc -l)
+linesss=$(cat /etc/SSHPLUS/RegV2ray | wc -l)
 	      echo -e "$contador_secuencial \n Número de registrados: $linesss"
 	}
 fi
@@ -245,8 +245,8 @@ msg -bar
 ###VER
 estarts () {
 VPSsec=$(date +%s)
-local HOST="/etc/VPS-MX/v2ray/lisportt.log"
-local HOST2="/etc/VPS-MX/v2ray/lisportt.log"
+local HOST="/etc/SSHPLUS/v2ray/lisportt.log"
+local HOST2="/etc/SSHPLUS/v2ray/lisportt.log"
 local RETURN="$(cat $HOST|cut -d'|' -f2)"
 local IDEUUID="$(cat $HOST|cut -d'|' -f1)"
 if [[ -z $RETURN ]]; then
@@ -256,9 +256,9 @@ ${SCPinst}/v2ray.sh
 else
 i=1
 while read hostreturn ; do
-iptables -n -v -L > /etc/VPS-MX/v2ray/data1.log 
-statsss=$(cat /etc/VPS-MX/v2ray/data1.log|grep -w "tcp spt:$hostreturn quota:"|cut -d' ' -f3,4,5)
-gblim=$(cat /etc/VPS-MX/v2ray/lisportt.log|grep -w "$hostreturn"|cut -d'|' -f2)
+iptables -n -v -L > /etc/SSHPLUS/v2ray/data1.log 
+statsss=$(cat /etc/SSHPLUS/v2ray/data1.log|grep -w "tcp spt:$hostreturn quota:"|cut -d' ' -f3,4,5)
+gblim=$(cat /etc/SSHPLUS/v2ray/lisportt.log|grep -w "$hostreturn"|cut -d'|' -f2)
 local contador_secuencial+="         \e[97mPORTA: \e[93m$hostreturn \e[97m|\e[93m$statsss \e[97m|\e[93m $gblim GB  \n"          
       if [[ $i -gt 30 ]]; then
 	      echo -e "$contador_secuencial"
@@ -269,7 +269,7 @@ let i++
 done <<< "$IDEUUID"
 
 [[ ! -z $contador_secuencial ]] && {
-linesss=$(cat /etc/VPS-MX/v2ray/lisportt.log | wc -l)
+linesss=$(cat /etc/SSHPLUS/v2ray/lisportt.log | wc -l)
 	      echo -e "$contador_secuencial \n Portas Limitadas: $linesss"
 	}
 fi
@@ -310,7 +310,7 @@ iptables-save > /etc/iptables/rules.v4
 echo ""
 echo -e " Porta Selecionada: $portbg | Quantidade de GB: $gbuser"
 echo ""
-echo " $portbg | $gbuser | $multiplicacion " >> /etc/VPS-MX/v2ray/lisportt.log 
+echo " $portbg | $gbuser | $multiplicacion " >> /etc/SSHPLUS/v2ray/lisportt.log 
 msg -bar
 msg -ne "Enter Para Continuar" && read enter
 ${SCPinst}/v2ray.sh
@@ -318,8 +318,8 @@ ${SCPinst}/v2ray.sh
 ###RES
 resdata () {
 VPSsec=$(date +%s)
-local HOST="/etc/VPS-MX/v2ray/lisportt.log"
-local HOST2="/etc/VPS-MX/v2ray/lisportt.log"
+local HOST="/etc/SSHPLUS/v2ray/lisportt.log"
+local HOST2="/etc/SSHPLUS/v2ray/lisportt.log"
 local RETURN="$(cat $HOST|cut -d'|' -f2)"
 local IDEUUID="$(cat $HOST|cut -d'|' -f1)"
 if [[ -z $RETURN ]]; then
@@ -328,9 +328,9 @@ return 0
 else
 i=1
 while read hostreturn ; do
-iptables -n -v -L > /etc/VPS-MX/v2ray/data1.log 
-statsss=$(cat /etc/VPS-MX/v2ray/data1.log|grep -w "tcp spt:$hostreturn quota:"|cut -d' ' -f3,4,5)
-gblim=$(cat /etc/VPS-MX/v2ray/lisportt.log|grep -w "$hostreturn"|cut -d'|' -f2)
+iptables -n -v -L > /etc/SSHPLUS/v2ray/data1.log 
+statsss=$(cat /etc/SSHPLUS/v2ray/data1.log|grep -w "tcp spt:$hostreturn quota:"|cut -d' ' -f3,4,5)
+gblim=$(cat /etc/SSHPLUS/v2ray/lisportt.log|grep -w "$hostreturn"|cut -d'|' -f2)
 local contador_secuencial+="         \e[97mPORTA: \e[93m$hostreturn \e[97m|\e[93m$statsss \e[97m|\e[93m $gblim GB  \n"  
         
       if [[ $i -gt 30 ]]; then
@@ -342,7 +342,7 @@ let i++
 done <<< "$IDEUUID"
 
 [[ ! -z $contador_secuencial ]] && {
-linesss=$(cat /etc/VPS-MX/v2ray/lisportt.log | wc -l)
+linesss=$(cat /etc/SSHPLUS/v2ray/lisportt.log | wc -l)
 	      echo -e "$contador_secuencial \n Portas Limitadas: $linesss"
 	}
 fi
@@ -365,13 +365,13 @@ echo -e "\e[91m                PORTA INVÁLIDA \n$(msg -bar)"
 msg -ne "Enter Para Continuar" && read enter
 ${SCPinst}/v2ray.sh
 }
-[[ $(sed -n '/'${portbg}'/=' /etc/VPS-MX/v2ray/lisportt.log|head -1) ]] || invaliduuid
-gblim=$(cat /etc/VPS-MX/v2ray/lisportt.log|grep -w "$portbg"|cut -d'|' -f3)
+[[ $(sed -n '/'${portbg}'/=' /etc/SSHPLUS/v2ray/lisportt.log|head -1) ]] || invaliduuid
+gblim=$(cat /etc/SSHPLUS/v2ray/lisportt.log|grep -w "$portbg"|cut -d'|' -f3)
 sudo iptables -D OUTPUT -p tcp --sport $portbg -j DROP
 sudo iptables -D OUTPUT -p tcp --sport $portbg -m quota --quota $gblim -j ACCEPT
 iptables-save > /etc/iptables/rules.v4
-lineP=$(sed -n '/'${portbg}'/=' /etc/VPS-MX/v2ray/lisportt.log)
-sed -i "${linePre}d" /etc/VPS-MX/v2ray/lisportt.log
+lineP=$(sed -n '/'${portbg}'/=' /etc/SSHPLUS/v2ray/lisportt.log)
+sed -i "${linePre}d" /etc/SSHPLUS/v2ray/lisportt.log
 msg -bar
 msg -ne "Enter Para Continuar" && read enter
 ${SCPinst}/v2ray.sh 
